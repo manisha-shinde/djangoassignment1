c0508b3e02d8c57f596cf635dc8de565d508c841
manisha-shinde <54435497+manisha-shinde@users.noreply.github.com>
2020-07-05 20:53:14 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('college/', include("college.urls")),
+
+    path('cse',views.cse,name="cse"),
+    path('etc',views.etc,name="etc"),
+    path('mech',views.mech,name="mech"),
+    path('civil',views.civil,name="civil"),
+
 
 ]
